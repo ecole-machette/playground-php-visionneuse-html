9461baf9264381512d2d64c21d4c0b397e25f23f
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-12 13:22:39 +0000

    Immeuble en entier

--- php-template/exercice3.php
+++ php-template/exercice3.php
@@ -1,3 +1,11 @@
 <img class="immeuble"
-     src="../images/immeuble-sol.jpeg"
+     src="https://raw.githubusercontent.com/ecole-machette/playground-u4fi09wi/master/images/immeuble-toit.jpeg"
+     alt="Rez-de-chaussée et premier étage de l'immeuble">
+
+<img class="immeuble"
+     src="https://raw.githubusercontent.com/ecole-machette/playground-u4fi09wi/master/images/immeuble-etage.jpeg"
+     alt="Rez-de-chaussée et premier étage de l'immeuble">
+
+<img class="immeuble"
+     src="https://raw.githubusercontent.com/ecole-machette/playground-u4fi09wi/master/images/immeuble-sol.jpeg"
      alt="Rez-de-chaussée et premier étage de l'immeuble">
