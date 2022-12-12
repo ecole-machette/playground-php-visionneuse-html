140840fe36364a0e6e572bcf0903fa6477192e42
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-12 19:46:02 +0000

    qsd

--- php-template/exercice3_test.php
+++ php-template/exercice3_test.php
@@ -1,9 +1,9 @@
 <?php
-    $string = include 'exercice3.php';
-echo $string;
+    include 'exercice3.php';
+    $string = "azer";
 
     $nb_string = mb_substr_count($string, "immeuble-etage.jpeg");
-echo $nb_string;
+    echo $nb_string;
     if($nb_string == 3){
         echo("TECHIO> message --channel Bravo! Tu as trouvé. \r\n");
     } else {
