06753c8abebbdeae75a95d8d1fc331969945440e
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-12 20:18:43 +0000

    fonctionne

--- markdowns/exercice3.md
+++ markdowns/exercice3.md
@@ -13,4 +13,4 @@ Ajoutez 2 étages à l'immeuble.
 @[Construire un immeuble]({"stubs": ["exercice3.php"], "command": "/bin/bash run.sh exercice3_test.php"})
 ```
 
-@[Construire un immeuble]({"stubs": ["exercice3.php","exercice3_test.php"], "command": "/bin/bash run.sh exercice3_test.php"})
+@[Construire un immeuble]({"stubs": ["exercice3.php"], "command": "/bin/bash run.sh exercice3_test.php"})
--- php-template/exercice3_test.php
+++ php-template/exercice3_test.php
@@ -1,13 +1,13 @@
 <?php
     include 'exercice3.php';
 
-    echo "</br> \r\n";
+    // echo "</br> \r\n";
     $nb_string = mb_substr_count($var, "immeuble-etage.jpeg");
-    echo $nb_string;
-    echo "</br> \r\n";
+    // echo $nb_string;
+    // echo "</br> \r\n";
 
     if($nb_string == 3){
-        echo("<p class='message'>" . "TECHIO> message --channel Bravo! Tu as trouvé. \r\n" . "</p>");
+        echo("TECHIO> message --channel Bravo! Tu as trouvé. \r\n");
     } else {
         // echo("<p class='message'>" . "TECHIO> success false  \r\n" . "</p>");
 		// echo("<p class='message'>" . "TECHIO> message --channel Perdu! Regarde bien quelle image correspond à un étage. \r\n" . "</p>");
