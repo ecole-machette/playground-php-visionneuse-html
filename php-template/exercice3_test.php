06b81e09af9f51b1779339780be4afffc704683d
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-12 20:12:32 +0000

    a

--- php-template/exercice3_test.php
+++ php-template/exercice3_test.php
@@ -1,6 +1,6 @@
 <?php
     include 'exercice3.php';
-    
+
     echo "</br> \r\n";
     $nb_string = mb_substr_count($var, "immeuble-etage.jpeg");
     echo $nb_string;
@@ -8,12 +8,11 @@
 
     if($nb_string == 3){
         echo("<p class='message'>" . "TECHIO> message --channel Bravo! Tu as trouvé. \r\n" . "</p>");
-        echo "</br> \r\n";
-
     } else {
-        echo("<p class='message'>" . "TECHIO> success false  \r\n" . "</p>");
-        echo "</br> \r\n";
-		echo("<p class='message'>" . "TECHIO> message --channel Perdu! Regarde bien quelle image correspond à un étage. \r\n" . "</p>");
+        // echo("<p class='message'>" . "TECHIO> success false  \r\n" . "</p>");
+		// echo("<p class='message'>" . "TECHIO> message --channel Perdu! Regarde bien quelle image correspond à un étage. \r\n" . "</p>");
+        echo("TECHIO> success false  \r\n");        
+		echo("TECHIO> message --channel Perdu! Regarde bien quelle image correspond à un étage. \r\n");
 
     }
 ?>
