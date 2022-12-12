e63a1fbf43f0e6d49008fc3428497523a80229d1
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-12 19:13:29 +0000

    correction alt

--- php-template/exercice3.php
+++ php-template/exercice3.php
@@ -6,11 +6,11 @@
 
 <img class="immeuble"
      src="https://raw.githubusercontent.com/ecole-machette/playground-u4fi09wi/master/images/immeuble-toit.jpeg"
-     alt="Rez-de-chaussée et premier étage de l'immeuble">
+     alt="Toit de l'immeuble">
 
 <img class="immeuble"
      src="https://raw.githubusercontent.com/ecole-machette/playground-u4fi09wi/master/images/immeuble-etage.jpeg"
-     alt="Rez-de-chaussée et premier étage de l'immeuble">
+     alt="Étage de l'immeuble">
 
 <img class="immeuble"
      src="https://raw.githubusercontent.com/ecole-machette/playground-u4fi09wi/master/images/immeuble-sol.jpeg"
