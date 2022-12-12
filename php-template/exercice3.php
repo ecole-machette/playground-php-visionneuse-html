2b7d152519af60ff13c8fd90280de7aa9cd2afed
ecole-machette <104460833+ecole-machette@users.noreply.github.com>
2022-12-12 13:31:33 +0000

    Correction css

--- php-template/exercice3.php
+++ php-template/exercice3.php
@@ -1,3 +1,9 @@
+<style>
+    img.immeuble {
+        display: block;
+    }
+</style>
+
 <img class="immeuble"
      src="https://raw.githubusercontent.com/ecole-machette/playground-u4fi09wi/master/images/immeuble-toit.jpeg"
      alt="Rez-de-chaussée et premier étage de l'immeuble">
